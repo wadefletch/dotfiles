0f7850f03f5311162433d28293b9786f3f54ad68
wadefletch <wbfletch@gmail.com>
2025-10-03 11:52:18 -0500

    2025-10-03T16:52:18

---
+++ ghostty/open-new-ghostty-window.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title Open New Ghostty Window
+# @raycast.mode silent
+
+# Optional parameters:
+# @raycast.icon 🤖
+# @raycast.packageName wadefletch
+
+# Documentation:
+# @raycast.author wadefletch
+# @raycast.authorURL https://raycast.com/wadefletch
+
+open -a "Ghostty"
