0214f67ba3210635bbd3185d8df3edad8472b3d4
Wade Fletcher <3798059+wadefletch@users.noreply.github.com>
2026-06-05 23:41:23 -0500

    feat(nvim,ghostty): adopt Xcode Dark HC theme (#45)
    * feat(nvim,ghostty): adopt Xcode Dark HC theme with transparent bg
    
    Switch nvim colorscheme to xcodedarkhc (lunacookies/vim-colors-xcode) and
    clear Normal's background for a transparent editor. Match ghostty's theme to
    Xcode Dark HC and drop background-opacity to 0.90.
    
    * fix(nvim): update renamed biome extra path
    
    LazyVim moved the extra from formatting.biome to lang.typescript.biome.

--- nvim/.config/nvim/lua/plugins/extras.lua
+++ nvim/.config/nvim/lua/plugins/extras.lua
@@ -10,13 +10,13 @@ return {
   { import = "lazyvim.plugins.extras.lang.terraform" },
   { import = "lazyvim.plugins.extras.lang.toml" },
   { import = "lazyvim.plugins.extras.lang.typescript" },
+  { import = "lazyvim.plugins.extras.lang.typescript.biome" },
   { import = "lazyvim.plugins.extras.lang.yaml" },
 
   -- plugins
   { import = "lazyvim.plugins.extras.coding.blink" },
   { import = "lazyvim.plugins.extras.coding.mini-comment" },
   { import = "lazyvim.plugins.extras.coding.mini-surround" },
-  { import = "lazyvim.plugins.extras.formatting.biome" },
   { import = "lazyvim.plugins.extras.formatting.prettier" },
   { import = "lazyvim.plugins.extras.linting.eslint" },
   { import = "lazyvim.plugins.extras.ui.mini-animate" },
